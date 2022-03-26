6f337f7d0e6e7dc764712949fbc3653bea90cbed
Vikram Mevasiya <vikrammevasiya@gmail.com>
2022-03-26 14:06:18 +0530

    Updates

---
+++ precache-manifest.f4a2430251d12cf8cfb9c2afbfe592c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08ec8e6d5ecb8f280703",
+    "url": "/static/css/main.26a9b9a3.chunk.css"
+  },
+  {
+    "revision": "08ec8e6d5ecb8f280703",
+    "url": "/static/js/main.bb1f356d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "789a87d873e5e4ecd401",
+    "url": "/static/js/2.f32daa48.chunk.js"
+  },
+  {
+    "revision": "88c0bfe601b9a006b29971a4f6295fb0",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ad79ba28c7747776aa22204643ce614d.js"
+  "/precache-manifest.f4a2430251d12cf8cfb9c2afbfe592c0.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
